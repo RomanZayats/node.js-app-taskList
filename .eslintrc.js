b4baa60943eb16548aa9ca7415f769c799c78b8d
Roman Zayats <zayatr01@kits.kfplc.com>
2023-08-16 15:07:47 +0300

    added linters

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: ["eslint:recommended", "prettier"],
+    env: {
+        node: true,
+        browser: true,
+        commonjs: true,
+        es2021: true,
+    },
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+    },
+    rules: {},
+};
