d509f7cf44d7331efce1405bc342c344ba779528
Roman Zayats <zayats.roma@gmail.com>
2023-08-16 17:48:20 +0300

    implemented mySQL model, GET and POST

--- public/app.js
+++ public/app.js
@@ -8,19 +8,33 @@ new Vue({
       todos: [],
     };
   },
+  created() {
+    fetch("/api/todo", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((todos) => {
+        this.todos = todos;
+      })
+      .catch((e) => console.log(e));
+  },
   methods: {
     addTodo() {
       const title = this.todoTitle.trim();
       if (!title) {
         return;
       }
-      this.todos.push({
-        title: title,
-        id: Math.random(),
-        done: false,
-        date: new Date(),
-      });
-      this.todoTitle = "";
+      fetch("/api/todo", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title }),
+      })
+        .then((res) => res.json())
+        .then(({ todo }) => {
+          this.todos.push(todo);
+          this.todoTitle = "";
+        })
+        .catch((e) => console.log(e));
     },
     removeTodo(id) {
       this.todos = this.todos.filter((t) => t.id !== id);
@@ -30,12 +44,18 @@ new Vue({
     capitalize(value) {
       return value.toString().charAt(0).toUpperCase() + value.slice(1);
     },
-    date(value) {
-      return new Intl.DateTimeFormat("us-US", {
+    date(value, withTime) {
+      const options = {
         year: "numeric",
         month: "long",
         day: "2-digit",
-      }).format(new Date(value));
+      };
+      if (withTime) {
+        options.hour = "2-digit";
+        options.minute = "2-digit";
+        options.second = "2-digit";
+      }
+      return new Intl.DateTimeFormat("us-US", options).format(new Date(value));
     },
   },
 });
--- routes/todo.js
+++ routes/todo.js
@@ -1,15 +1,54 @@
 const { Router } = require("express");
 
+const Todo = require("../models/todo");
+
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.json({ a: 1 });
+router.get("/", async (req, res) => {
+  try {
+    const todos = await Todo.findAll();
+    res.status(200).json(todos);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 });
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    const todo = await Todo.create({
+      title: req.body.title,
+      done: false,
+    });
+    res.status(201).json({ todo });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  try {
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  try {
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
 
 module.exports = router;
