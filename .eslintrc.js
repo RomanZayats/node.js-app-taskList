7193874a5a0c539817e40a9444c8449a2e70da7e
Roman Zayats <zayatr01@kits.kfplc.com>
2023-08-16 15:09:22 +0300

    trivial update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    extends: ["eslint:recommended", "prettier"],
-    env: {
-        node: true,
-        browser: true,
-        commonjs: true,
-        es2021: true,
-    },
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: "latest",
-    },
-    rules: {},
+  extends: ["eslint:recommended", "prettier"],
+  env: {
+    node: true,
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
 };
